0b1de4fcd66ee1e0451dddffa60a0a61bcd18c8c
Hans Brenna <hans.brenna@geo.uio.no>
2015-06-15 09:33:59 +0200

    Point plot working with help text. started the process of developing bandplot

--- plotter2.py
+++ plotter2.py
@@ -6,6 +6,7 @@ Created on Wed Jun 03 16:03:10 2015
 """
 
 #Import required modules
+from __future__ import print_function
 import sys
 import numpy as nmp
 from mpl_toolkits.basemap import Basemap
@@ -15,8 +16,15 @@ import matplotlib.colors as colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.colors import Normalize
 from netCDF4 import Dataset
+import seaborn as sns
 
 from IPython import embed
+
+sns.set(style="dark")
+
+def find_nearest(array,value):
+    idx = (nmp.abs(array-value)).argmin()
+    return array[idx]
 
 class MidpointNormalize(Normalize):
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
@@ -32,11 +40,11 @@ class MidpointNormalize(Normalize):
 def ExtractVariables(ID,var,xax,yax):
     x = id_in.variables[xax][:] ; # extracting x axis variable 1d
     xl = size(x)
-    print shape(x)
+    print( shape(x))
     
     y = id_in.variables[yax][:] ; # extracting y axis variable 1d
     yl = size(y)
-    print shape(y)
+    print( shape(y))
     
     xunits = id_in.variables[xax].units
     yunits = id_in.variables[yax].units
@@ -61,13 +69,44 @@ def ExtractVariables(ID,var,xax,yax):
     if yax == 'lon':
         ax.remove(3)
     
-    print ax    
+    print( ax)
     
     P = id_in.variables[var][:,:,:,:] ; # extracting 4D variable "var" (3D+T field) P=generic
     Punits = id_in.variables[var].units
 #    embed()
     return x,y,P,Punits,ax,xunits,yunits
-
+    
+def plotfunc(var,mP,x,y,xunits,yunits,Punits):
+    """
+    docstring   
+    """
+    wo = 0.95 ; # width occupation for each figure (fraction)    
+    
+    xis = axes([0.09,  0.1,   0.85,       0.82], axisbg = 'white')
+    
+    xx,yy=nmp.meshgrid(x,y)
+    print( 'Shape of meshgrid x and y axes is ',shape(xx), shape(yy))
+    if shape(xx) != shape(mP):
+        mP = transpose(mP)
+        print( 'Shapes were unequal, so variable is transposed. New shape ', shape(mP))
+        if shape(xx) != shape(mP):
+            print( 'Shapes still unequal. Exiting...')
+            sys.exit()
+    
+    if var == 'O3' or var == 'T':
+        CF = contourf(x,y,mP,10,cmap=matplotlib.cm.copper)
+        CS=contour(x, y, mP,10,colors='k')
+    else:
+        norm = MidpointNormalize(midpoint=0)
+        CF = contourf(x,y,mP,linspace(nmp.amin(mP),nmp.amax(mP),1000),norm=norm,cmap='seismic')    
+        CS=contour(x, y, mP,10,colors='k')
+    
+    axis([min(x), max(x), max(y), min(y)])
+    xlabel(xunits); ylabel(yunits);
+    clb = colorbar(CF); clb.set_label('('+Punits+')')
+    #clabel(CS,inline=1,fontsize=8)
+    return
+      
 def figplot(ID, var, xax, yax):
 
     fig = figure(num = 1, figsize=(10.,5.), dpi=None, facecolor='w', edgecolor='k')
@@ -85,50 +124,95 @@ def figplot(ID, var, xax, yax):
     
 #    embed()
     mP = nmp.mean(nmp.mean(P[:,:,:,:],axis=ax[1]),axis=ax[0])
-    print 'Shape of averaged variable array for plotting is',shape(mP)
+    print( 'Shape of averaged variable array for plotting is',shape(mP))
     
-    xx,yy=nmp.meshgrid(x,y)
-    print 'Shape of meshgrid x and y axes is ',shape(xx), shape(yy)
-    if shape(xx) != shape(mP):
-        mP = transpose(mP)
-        print 'Shapes were unequal, so variable is transposed. New shape ', shape(mP)
-        if shape(xx) != shape(mP):
-            print 'Shapes still unequal. Exiting...'
-            sys.exit()
+    fig = figure(num = 1, figsize=(10.,5.), dpi=None, facecolor='w', edgecolor='k')
+    plotfunc(var,mP,x,y,xunits,yunits,Punits)
     
-    if var == 'O3' or var == 'T':
-        CF = contourf(x,y,mP,10)
-        CS=contour(x, y, mP,10,colors='k')
-    else:
-        norm = MidpointNormalize(midpoint=0)
-        CF = contourf(x,y,mP,linspace(nmp.amin(mP),nmp.amax(mP),1000),norm=norm,cmap='seismic')    
-        CS=contour(x, y, mP,10,colors='k')
-    
-    axis([min(x), max(x), max(y), min(y)])
-    xlabel(xunits); ylabel(yunits);
-    clb = colorbar(CF); clb.set_label('('+Punits+')')
-    #clabel(CS,inline=1,fontsize=8)
-    
-    savefig(cf_in+var+xax+yax+'.png', dpi=100, facecolor='w', edgecolor='w', orientation='portrait')
-    print cf_in+var+xax+yax+'.png was saved' 
+    savefig(cf_in+var+xax+yax+'.png', dpi=100, facecolor='w', 
+            edgecolor='w', orientation='portrait')
+    print("{0}.png was {1:2.3f} saved".format(cf_in+var+xax+yax,1.52248621))
 
     close(fig)    
     
     return 0    
     
-def bandplot(ID, var, xax, yax,):
-    print test
+def bandplot(ID, var, xax, yax,dim,dmin,dmax):
+    x,y,P,Punits,ax,xunits,yunits=ExtractVariables(ID,var,xax,yax)
+    d1 = id_in.variables[dim][:]
+    #embed()
+    print( d1)
+    
+    nmin = find_nearest(d1,dmin)
+    print( nmin)
+    nmax = find_nearest(d2,dmax)
+    print( nmax)
+    
+    
+
     return 0
     
-def pointplot(ID, var, xax, yax,p1,p2):
+def pointplot(ID, var, xax, yax,dim1,p1,dim2,p2):
+    x,y,P,Punits,ax,xunits,yunits=ExtractVariables(ID,var,xax,yax)
+    d1 = id_in.variables[dim1][:]
+    #embed()
+    print( d1)
+    d2 = id_in.variables[dim2][:]
+    print( d2)
+    
+    nearest1 = find_nearest(d1,p1)
+    print( nearest1)
+    nearest2 = find_nearest(d2,p2)
+    print( nearest2)
+    
+    print( 'p1=' ,p1)
+    #embed()
+    index1 = nmp.where(d1==nearest1)
+    print( index1)
+    ind1 = index1[0][0]
+    index2 = nmp.where(d2==nearest2)
+    ind2 = index2[0][0]
+
+    condition1 = zeros(shape = shape(d1))
+    #embed()
+    condition1[ind1]=1  
+    condition2 = zeros(shape = shape(d2))
+    condition2[ind2]=1 
+
+    print( P.shape    )
+    
+    xP = nmp.compress(condition1,P,axis=ax[0])
+    print( xP.shape)
+    xP = nmp.compress(condition2,xP,axis=ax[1])
+    print( xP.shape)
+    xP = squeeze(xP)
+    print( xP.shape)
+    
+    fig = figure(num = 1, figsize=(10.,5.), dpi=None, facecolor='w', edgecolor='k')
+
+    wo = 0.95 ; # width occupation for each figure (fraction)    
+    
+    xis = axes([0.09,  0.1,   0.85,       0.82], axisbg = 'white')
+    if yax == 'lev':
+        xis.set_yscale("log")
+    
+    plotfunc(var,xP,x,y,xunits,yunits,Punits)
+    
+    savefig('{0}{1}{2}{3}_point_{4}={5:2.3f}{6}={7:2.3f}.png'.format(cf_in,var,xax,yax,dim1,nearest1,dim2,nearest2),
+            dpi=100, facecolor='w', edgecolor='w', orientation='portrait')
+    print( cf_in+var+xax+yax+'.png was saved' )
+
+    close(fig)   
     return 0
 
 if __name__ == "__main__":
     avgall=False; bandavg=False; point=False;
-    if len(sys.argv) < 5:
-        print 'This script takes at least 5 command line arguments ',len(sys.argv),' is given. \n'
-        print 'The usage is: Name of this script; path and name of netcdf file to be analysed;\n'
-        print 'name of variable; name of x-axis; name of y-axis (time, lev, lat, lon)'
+    if len(sys.argv) < 5 or 'help' in sys.argv:
+        print( 'This script takes at least 5 command line arguments ',len(sys.argv),' is given. \n')
+        print( 'The usage is: Name of this script; path and name of netcdf file to be analysed;\n')
+        print( 'name of variable; name of x-axis; name of y-axis (time, lev, lat, lon)')
+        print( 'The 6th argumaent must be either point or band. If point')
+        print( 'a point must be specified in the other two dimensions on the form (dim1 point1 dim2 point2)')
         sys.exit()
     elif len(sys.argv) == 5:
         avgall = True
@@ -139,36 +223,36 @@ if __name__ == "__main__":
             point = True
         if sys.argv[5] == 'point':
             point = True
-            point1 = sys.argv[6]
-            point2 = sys.argv[7]
+            dim1 = sys.argv[6]
+            point1 = double(sys.argv[7])
+            dim2 = sys.argv[8]
+            point2 = double(sys.argv[9])
         else:
-            print 'If this script is given more than 5 command line arguments, sys.argv[5] has to be "cut", "point" or "band"'
+            print( "If this script is given more than 5 command line arguments, sys.argv[5] has to be 'cut', 'point' or 'band'")
             sys.exit()
-            
-        
-    
+              
     cf_in = sys.argv[1]
     
     id_in = Dataset(cf_in)
-    print 'File ', cf_in, 'is open...\n'
+    print( 'File ', cf_in, 'is open...\n')
     
     variable = sys.argv[2]
     if variable not in id_in.variables:
-        print 'no such variable in ', cf_in, '\n'
-        print id_in.variables
+        print( 'no such variable in ', cf_in, '\n')
+        print( id_in.variables)
         sys.exit()
     
     xax = sys.argv[3] #Usage: time, lev, lat, lon
     yax = sys.argv[4]
     
     if xax==yax:
-        print 'x-axis and y-axis are the same variable'
+        print( 'x-axis and y-axis are the same variable')
         sys.exit()
     
     if avgall:
         figplot(id_in,variable,xax,yax)
 
     if point:
-        pointplot(id_in,variable,xax,yax,point1,point2)
+        pointplot(id_in,variable,xax,yax,dim1,point1,dim2,point2)
     
     
