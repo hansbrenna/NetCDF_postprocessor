84754db8cc1700250289f99f05c04f66ad5b0bc9
Hans Brenna <hans.brenna@geo.uio.no>
2015-06-18 15:47:25 +0200

    New version, more general, takes an input file with values to change

---
+++ restart_file_modifier2.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 10:05:03 2015
+
+@author: hanbre
+"""
+
+from __future__ import print_function
+import sys
+import numpy as np
+import pandas as pd
+import xray
+import netCDF4
+
+
+def read_data(id_in):
+    data = xray.open_dataset(id_in)
+    return data
+    
+def modify_data(ds,var,le,la,lo,value):
+    getattr(ds,var)[dict(lat=la,lon=lo,lev=le)]=getattr(ds,var)[dict(lat=la,lon=lo,lev=le)]*100
+    return ds
+    
+
+id_in = sys.argv[1]
+case_id = id_in.split('/')
+f1=open('{0}/modified/{1}_README'.format(case_id[0],case_id[1]),'w')
+ds = read_data(id_in)
+with open(sys.argv[2], 'r') as file_in:
+    header=next(file_in)
+    for line in file_in:
+        l=line.strip('\n').split(' ')
+        le = int(l[0]); la = int(l[1]); lo = int(l[2])
+        var = str(l[3])
+        value = float(l[4])
+        print(ds.HCL[dict(lat=la,lon=lo,lev=le)].values)
+        ds=modify_data(ds,var,le,la,lo,value)
+        print(ds.HCL[dict(lat=la,lon=lo,lev=le)].values)
+        print('file {0}/modified/{1} has been modified by program restart_file_modifier2.py'.format(case_id[0],case_id[1]),file=f1)
+        print('The changes take effect at lev={0}, lat={1}, lon={2} and the changed value is {3}={4}'.format(le,la,lo,getattr(ds,var).name,getattr(ds,var)[dict(lat=la,lon=lo,lev=le)].values),file=f1)
+
+ds.to_netcdf(path='{0}/modified/{1}'.format(case_id[0],case_id[1]),mode='w',)
+f1.close()
